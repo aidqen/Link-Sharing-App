feb456e625d43300efe864ee9b2c2459fc4e701b
aidqen <idanmarkin8@gmail.com>
2024-08-03 13:11:05 +0300

    Add default img if no img is present in profileDetails

--- Frontend/src/cmps/PhonePreview/PhonePreviewDetails.jsx
+++ Frontend/src/cmps/PhonePreview/PhonePreviewDetails.jsx
@@ -4,21 +4,20 @@ import { SocialMediaLabel } from './SocialMediaLabel'
 export function PhonePreviewDetails() {
   const linksList = useSelector(state => state.linksList)
   const profileDetails = useSelector(state => state.personalDetails)
-  const { email, firstName, lastName } = profileDetails
+  const { email, firstName, lastName, img } = profileDetails
 
   return (
     <div className="flex flex-col justify-start items-center bg-white py-5 px-6 h-full w-full overflow-y-auto custom-scrollbar">
       <img
-        src="src/assets/images/hi.jpg"
-        className="mb-6 rounded-[50%] border-4 border-purple-bright w-[8em] h-[8em] object-cover"
-        alt=""
+        src={img ? img : 'src/assets/images/random-person.jpg'}
+        className="mb-6 rounded-[50%] border-4 border-purple-bright min-w-[8em] min-h-[8em] max-w-[8em] max-h-[8em] object-cover"
+        alt="Picked image by user"
       />
       {firstName && lastName && (
         <h3 className="mb-2 font-semibold text-lg">
           {firstName} {lastName}
         </h3>
       )}
-      {email && <h4 className="text-sm text-slate-500 mb-10">{email}</h4>}
       {(!firstName || !lastName || !email) && (
         <div
           role="status"
@@ -32,6 +31,7 @@ export function PhonePreviewDetails() {
           )}
         </div>
       )}
+      {email && <h4 className="text-sm text-slate-500 mb-10">{email}</h4>}
       <div className="w-full h-full">
         {linksList &&
           linksList.map(link => {
