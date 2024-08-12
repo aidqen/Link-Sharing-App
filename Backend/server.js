30300129b2392aef541d9ce9503b8289f46bffc9
aidqen <idanmarkin8@gmail.com>
2024-08-12 15:37:35 +0300

    Add backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,46 +1,62 @@
-import express from 'express'
+import express  from 'express'
 import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import { linksService } from './services/links.service'
+import cors  from 'cors'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+import { logger } from './services/logger.service.js'
+logger.info('server.js loaded...')
 
 const app = express()
 
+// Express App Config
 app.use(cookieParser())
 app.use(express.json())
+app.use(express.static('public'))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('public'))
+    // Express serve static files on production environment
+    app.use(express.static(path.resolve(__dirname, 'public')))
+    console.log('__dirname: ', __dirname)
 } else {
-  const corsOptions = {
-    origin: [
-      'http://127.0.0.1:3000',
-      'http://localhost:3000',
-      'http://127.0.0.1:3030',
-      'http://localhost:3030',
-      'http://localhost:5173',
-      'http://127.0.0.1:5173',
-      'http://localhost:5174',
-      'http://127.0.0.1:5174',
-    ],
-    credentials: true,
-  }
-  app.use(cors(corsOptions))
+    // Configuring CORS
+    // Make sure origin contains the url 
+    // your frontend dev-server is running on
+    const corsOptions = {
+        origin: [
+            'http://127.0.0.1:5173', 
+            'http://localhost:5173',
+
+            'http://127.0.0.1:3000', 
+            'http://localhost:3000',
+        ],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
 }
 
-app.get('/api/links', async (req, res) => {
-    const links = linksService.query()
-    console.log('links:', links)
-    res.send(links)
-})
+import { authRoutes } from './api/auth/auth.routes.js'
+import { userRoutes } from './api/user/user.routes.js'
+import { carRoutes } from './api/car/car.routes.js'
 
-app.post('/api/links', async (req, res) => {
-    const link = req.body
-    console.log('link:', link)
-    res.send(link)
-})
+// routes
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/car', carRoutes)
 
+// Make every unmatched server-side-route fall back to index.html
+// So when requesting http://localhost:3030/index.html/car/123 it will still respond with
+// our SPA (single page app) (the index.html file) and allow vue-router to take it from there
+
+app.get('/**', (req, res) => {
+    res.sendFile(path.resolve('public/index.html'))
+})
 
 const port = process.env.PORT || 3030
+
 app.listen(port, () => {
-  loggerService.info(`Server listening on port http://127.0.0.1:${port}/`)
+    logger.info('Server is running on port: ' + port)
 })
